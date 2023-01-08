177108f03f564e95d1359b87770331eaef42d5e0
Kiran-pande-30 <105718863+Kiran-pande-30@users.noreply.github.com>
2023-01-08 19:16:11 +0530

    Update Questionaire.php

--- Questionaire.php
+++ Questionaire.php
@@ -125,10 +125,6 @@ textarea{
     }
 }
 
-
-
-
-
 </style>
 <body class="page-top-2">
     <header class="header">
@@ -168,27 +164,27 @@ textarea{
                 </select> 
             </div>
             <div class="details">
-                <div class="summary">What problem are you aiming to solve ?</div> 
+                <div class="summary">Provide a brief description of the problem indentified.</div> 
                 <textarea required name="q1" id="q1" cols="50" rows="5"  ></textarea>
             </div>
             <div class="details">
-                <div class="summary">Have you researched this problem in the field? If yes, how was the response?</div> 
+                <div class="summary">Provide a brief synopsis of the solution developed.</div> 
                 <textarea name="q2" id="q2" cols="50" rows="5"  ></textarea>
             </div>
             <div class="details">
-                <div class="summary">What is your target customer segment and describe your customer persona.</div>
+                <div class="summary">Provide a detailed use case of your idea.</div>
                 <textarea required name="q3" id="q3" cols="50" rows="5"  ></textarea>
             </div>
             <div class="details">
-                <div class="summary">Are you providing a service or product to solve the problem? Describe the product/service.</div>
+                <div class="summary">Mention details about your market sizing and target customer segment.</div>
                 <textarea required name="q4" id="q4" cols="50" rows="5"  ></textarea>
             </div>
             <div class="details">
-                <div class="summary">What is your model for revenue generation? Explain the revenue model in brief.</div> 
+                <div class="summary">Are there any presently operational startups serving the same problem? If yes, mention the differentiators your company holds.(Mention scope of patency or other intellectual property rights, if any). </div> 
                 <textarea required name="q5" id="q5" cols="50" rows="5"  ></textarea>
             </div>
             <div class="details">
-                <div class="summary">Who are your direct and indirect competitors in the space currently and how do you differentiate yourself from them? (Mention scope of patency or other intellectual property rights, if any) (USP)
+                <div class="summary">Is your idea a revenue-generating one? Explain the revenue model in brief including the components that affect costings and prices
                 </div> 
                 <textarea required name="q6" id="q6" cols="50" rows="5"  ></textarea>
             </div>
@@ -198,11 +194,11 @@ textarea{
             </div>
 
             <div class="details">
-                <div class="summary">What was the revenue of your startup in the last few financial years? How many services/products did you sold in each year?</div> 
+                <div class="summary">What was the revenue of your startup in the last few financial years? What is the total volume of services/products sold till date?</div> 
                 <textarea required name="q7" id="q7" cols="50" rows="5"  ></textarea> 
             </div>
             <div class="details">
-                <div class="summary">From how long is your venture operational?</div> 
+                <div class="summary">What are the operational and marketing channels your venture uses? For how long is your venture operational? Brief about the growth that your venture has experienced (specifying the basis of growth)</div> 
                 <textarea required name="q8" id="q8" cols="50" rows="5"  ></textarea> 
             </div>
             <div class="details">
